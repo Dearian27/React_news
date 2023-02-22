5a13ac280831489e86ad94a4e42544234e8e7b5a
Dearian <dearian2706@gmail.com>
2023-02-22 21:26:15 +0200

    add follow alert

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Alert, Box, Button, Container, Snackbar, Typography } from "@mui/material";
 import Grid from '@mui/material/Grid'; // Grid version 1
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper/Paper";
@@ -20,16 +20,27 @@ const News: React.FC = () => {
 
   const [availablePosts, setAvailablePosts] = useState<postI[]>([]);
 
+  const [alert, setAlert] = useState<boolean>(false);
+
   const handleAddPosts = () => {
     setVisions(visions + 3);
   }
 
+  const handlePostClick = () => {
+    setAlert(true);
+  }
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlert(false);
+  };
+
   useEffect(() => {
     setAvailablePosts(posts.slice(0, visions));
-    // setAvailablePosts(posts)
   }, [posts, visions]);
 
-  // console.log("availablePosts", availablePosts);
 
   useEffect(() => {
     dispatch(getPosts());
@@ -47,7 +58,7 @@ const News: React.FC = () => {
           :
           availablePosts?.map((post) => {
             return (
-              <Grid item lg={4} xl={3} xs={12} sm={6}>
+              <Grid item key={post.id} lg={4} xl={3} xs={12} sm={6}>
                 <Paper elevation={4} sx={{ borderRadius: 3 }}>
                   <img style={{ width: "100%", height: "300px", objectFit: "cover", borderTopLeftRadius: "12px", borderTopRightRadius: "12px" }}
                     src={post.url}
@@ -57,7 +68,7 @@ const News: React.FC = () => {
                     <Typography variant="h5" sx={{ fontSize: "22px" }}>
                       {post.title}
                     </Typography>
-                    <Button sx={{ mt: 1 }} variant="contained">Follow</Button>
+                    <Button onClick={handlePostClick} sx={{ mt: 1 }} variant="contained">Follow</Button>
                   </Box>
                 </Paper>
               </Grid>
@@ -67,6 +78,11 @@ const News: React.FC = () => {
 
       </Grid>
       <Button onClick={handleAddPosts} size="large" variant="outlined">Show more</Button>
+      <Snackbar open={alert} autoHideDuration={3000} onClose={handleClose} sx={{ position: "fixed", ml: "50%", transform: "translateX(-50%)", bottom: "30px" }}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '350px' }}>
+          You followed the post!
+        </Alert>
+      </Snackbar>
     </section >
   )
 }
