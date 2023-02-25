7d61826211c75ca7b1bb5e36a6665c26911da756
Dearian <dearian2706@gmail.com>
2023-02-25 23:59:08 +0200

    fix problem with languages

--- src/18n.ts
+++ src/18n.ts
@@ -4,15 +4,15 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-
+const Ls = ["en", "ua", "de"];
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: ["en", "ua", "de"],
+  backend: {
+    loadPath: "/locales/{{lng}}/{{ns}}.json",
+  },
+  fallbackLng: Ls,
   debug: true,
-  detection: {
-      order: ["queryString", "cookie"],
-      // cache: ["cookie"],
-      },
+  // whitelist: Ls,
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
   }
